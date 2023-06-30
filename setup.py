a8c459c5d94f9d5674387b4f85ba60668cab6c0a
XiaoBaiYun <71159641+littlewhitecloud@users.noreply.github.com>
2023-06-30 20:26:14 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,12 +5,11 @@ with open("README.md", "r", encoding="utf-8") as file:
     long_description = file.read()
 
 setup(
-    name="",
-    version="",
-    description="",
+    name="translucenttbb",
+    version="0.0.1",
+    description="A python version TranslucentTB",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    author="",
-    url="",
-    packages=[""],
+    author="littlewhitecloud",
+    url="https://github.com/littlewhitecloud/TranslucentTB/",
 )
