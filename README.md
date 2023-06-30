d94ef8593f09a318ce05c7ed6f1e94b5298feea8
XiaoBaiYun <71159641+littlewhitecloud@users.noreply.github.com>
2023-06-30 20:18:53 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+### PyTemplate
+#### This is a Template that you can use it as template and create a python repo quickly.
+#### Click ```Use this template``` to quickly create a python repo.
---
+++ setup.py
@@ -0,0 +1,16 @@
+"""Setup"""
+from distutils.core import setup
+
+with open("README.md", "r", encoding="utf-8") as file:
+    long_description = file.read()
+
+setup(
+    name="",
+    version="",
+    description="",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="",
+    url="",
+    packages=[""],
+)
