bc9a5d71b3c5b901ff3985913b4f608a5f0d7cae
XiaoBaiYun <71159641+littlewhitecloud@users.noreply.github.com>
2023-07-04 16:00:42 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as file:
 
 setup(
     name="translucenttb",
-    version="0.0.3",
+    version="0.0.4",
     description="A python version TranslucentTB",
     long_description=long_description,
     long_description_content_type="text/markdown",
